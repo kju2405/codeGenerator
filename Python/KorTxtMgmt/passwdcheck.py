b1755d80a2df7c6e9e1184e4574151e381d36ee5
unknown <fooddeuk@kakao.com>
2022-05-31 14:45:10 +0900

    Error on passwdCheck() fixed.

--- Python/KorTxtMgmt/passwdcheck.py
+++ Python/KorTxtMgmt/passwdcheck.py
@@ -1,7 +1,7 @@
 import re
 
 def passwdCheck(passwd):
-    numCondition, capCondition, smlCondition, spcCondition = 0, 0, 0, 0
+    numCondition, capCondition, smlCondition, spcCondition, lenCondition = 0, 0, 0, 0, 0
     if len(re.findall('[0-9]', passwd)) > 0:                                    # 숫자 포함 여부
         numCondition = 1
     if len(re.findall('[A-Z]', passwd)) > 0:                                    # 대문자 포함 여부
